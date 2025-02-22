68daf5404a6947e3dbfd272be65984dd5df5ca07
Yahia Alhejoj <yahiaalhejoj@Macmini.lan>
2025-02-21 22:14:38 -0800

    initial 2

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Qubais</h1>
+      <h1>Qubais project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
